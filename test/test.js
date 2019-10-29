b9835c85345c2350036b0cd26306b8b8e0958ea0
kgauzampe <kgauza.mpe@gmail.com>
2019-10-29 14:43:54 +0200

    second commit

---
+++ test/test.js
@@ -0,0 +1,26 @@
+let calculator = require('../Calculator');
+
+describe("simple calculator to add two values", function () {
+    it("should add two values", function () {
+        expect(calculator.add(1,1)).toEqual(2);
+    });
+});
+
+describe("simple calculator to add multiple values", function () {
+    it("should add many values", function () {
+        expect(calculator.addMultiple(1,2,3,4)).toEqual(10);
+    });
+});
+
+describe("simple calculator to multiply two values", function () {
+    it("should multiply two values", function () {
+        expect(calculator.multiply(1,2)).toEqual(2);
+    });
+});
+
+
+describe("simple calculator to multiply two values", function () {
+    it("should multiply two values", function () {
+        expect(calculator.multipleMultiply(1,2,3,4)).toEqual(24);
+    });
+});
